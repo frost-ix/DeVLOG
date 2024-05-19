d261ef51487a1b0d89a9771963cea0b6e7f81708
frost-ix <shw9810@icloud.com>
2024-05-19 19:25:12 +0900

    Fix : gitignore

--- src/main/java/io/devlog/blog/oauth/service/OAuthService.java
+++
@@ -1,5 +0,0 @@
-package io.devlog.blog.oauth.service;
-
-public interface OAuthService {
-//    HashMap<String, String> getSocialTokens(String code);
-}
--- src/main/java/io/devlog/blog/security/properties/JwtProperty.java
+++
@@ -1,9 +0,0 @@
-package io.devlog.blog.security.properties;
-
-public interface JwtProperty {
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-    String SECRET = "blog_by_DeVLOG"; // 비밀값
-    int ACCESS_EXPIRATION_TIME = 60000 * 60; // 60분
-    int REFRESH_EXPIRATION_TIME = 60000 * 60 * 24 * 7; // 7일
-}
