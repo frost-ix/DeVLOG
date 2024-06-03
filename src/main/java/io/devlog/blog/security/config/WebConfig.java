3816a2261122239594e0ceec149db2c69c05efe4
frost-ix <shw9810@icloud.com>
2024-06-03 18:21:51 +0900

    fix : web cors

---
+++ src/main/java/io/devlog/blog/security/config/WebConfig.java
@@ -0,0 +1,15 @@
+package io.devlog.blog.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
