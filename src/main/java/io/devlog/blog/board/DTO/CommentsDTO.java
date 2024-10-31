660c727b79ccaa4c103a0abc0e9ee747457a1db0
Chan Ik Jo <wcd065864@gmail.com>
2024-10-31 14:38:36 +0900

    create: comments date usericon

--- src/main/java/io/devlog/blog/board/DTO/CommentsDTO.java
+++ src/main/java/io/devlog/blog/board/DTO/CommentsDTO.java
@@ -7,6 +7,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentsDTO {
@@ -16,15 +18,19 @@ public class CommentsDTO {
     private Long boardUuid;
     private Long userUuid;
     private String userName;
+    private LocalDateTime commentDate;
+    private String userIcon;
 
     @Builder
-    public CommentsDTO(@Nullable Long commentUuid, @Nullable String comments, @Nullable String imagePath, @Nullable Long boardUuid, @Nullable Long userUuid, @Nullable String userName) {
+    public CommentsDTO(@Nullable Long commentUuid, @Nullable String comments, @Nullable String imagePath, @Nullable Long boardUuid, @Nullable Long userUuid, @Nullable String userName, @Nullable LocalDateTime commentDate, @Nullable String userIcon) {
         this.commentUuid = commentUuid;
         this.comments = comments;
         this.imagePath = imagePath;
         this.boardUuid = boardUuid;
         this.userUuid = userUuid;
         this.userName = userName;
+        this.commentDate = commentDate;
+        this.userIcon = userIcon;
     }
 
     public static CommentsDTO toDTO(Comments comments) {
@@ -34,6 +40,8 @@ public class CommentsDTO {
                 .imagePath(comments.getImagePath())
                 .boardUuid(comments.getBoard().getBoardUuid())
                 .userName(comments.getUserName())
+                .commentDate(comments.getCommentDate())
+                .userIcon(null)
                 .build();
     }
 
@@ -45,6 +53,7 @@ public class CommentsDTO {
                 .userUuid(userUuid)
                 .userName(userName)
                 .board(null)
+                .commentDate(commentDate)
                 .build();
     }
 }
